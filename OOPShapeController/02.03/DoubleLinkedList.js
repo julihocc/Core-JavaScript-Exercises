50cac827602769ed5e930cd83bbffef34f4a7d6e
Juliho Castillo <julihocc@users.noreply.github.com>
2024-03-21 11:38:19 -0600

    Implement undo and redo functionality in History class

--- OOPShapeController/02.03/DoubleLinkedList.js
+++ OOPShapeController/02.03/DoubleLinkedList.js
@@ -1,17 +1,47 @@
-// Class representing a doubly linked list
+// Class representing the history of actions in an application,
+// for implementing undo and redo functionality.
 
-class Node {
-  constructor(value) {
-    this.value = value;
+class Action {
+  constructor(action) {
+    this.action = action;
     this.next = null;
     this.prev = null;
   }
 }
 
-class DoubleLinkedList {
+class History {
   constructor() {
     this.head = null;
     this.tail = null;
-    this.length = 0;
+    this.current = null;
+  }
+
+  addAction(action) {
+    if (this.head === null) {
+      this.head = action;
+      this.tail = action;
+      this.current = action;
+    } else {
+      this.current.next = action;
+      action.prev = this.current;
+      this.current = action;
+      this.tail = action;
+    }
+  }
+
+  undo() {
+    if (this.current.prev === null) {
+      return null;
+    }
+    this.current = this.current.prev;
+    return this.current.action;
+  }
+
+  redo() {
+    if (this.current.next === null) {
+      return null;
+    }
+    this.current = this.current.next;
+    return this.current.action;
   }
 }
