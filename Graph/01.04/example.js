8ba98329c9f40ee0a709bd8945bb179c8c9fbfae
Juliho Castillo <julihocc@users.noreply.github.com>
2024-03-25 12:37:04 -0600

    New folders for advanced trees

---
+++ Graph/01.04/example.js
@@ -0,0 +1,13 @@
+import Graph from "./Graph.js";
+
+const graph = new Graph();
+
+const nodeA = graph.addNode("A");
+const nodeB = graph.addNode("B");
+const nodeC = graph.addNode("C");
+
+graph.addEdge(nodeA, nodeB, 1);
+graph.addEdge(nodeB, nodeC, 2);
+graph.addEdge(nodeC, nodeA, 3);
+
+graph.display();
