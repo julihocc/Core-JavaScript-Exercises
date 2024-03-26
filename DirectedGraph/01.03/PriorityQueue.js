0893e82c922c7cc348552334185526a60478d39f
Juliho Castillo <julihocc@users.noreply.github.com>
2024-03-26 09:59:56 -0600

    Refactor DirectedGraph class to use Edge objects for representing edges

---
+++ DirectedGraph/01.03/PriorityQueue.js
@@ -0,0 +1,99 @@
+class PriorityQueue {
+  constructor() {
+    this.items = [];
+  }
+
+  // Swap elements at the indices i and j
+  swap(i, j) {
+    [this.items[i], this.items[j]] = [this.items[j], this.items[i]];
+  }
+
+  // Returns the index of the parent node
+  parentIndex(index) {
+    return Math.floor((index - 1) / 2);
+  }
+
+  // Returns the index of the left child node
+  leftChildIndex(index) {
+    return 2 * index + 1;
+  }
+
+  // Returns the index of the right child node
+  rightChildIndex(index) {
+    return 2 * index + 2;
+  }
+
+  // Returns true if the node at index has a parent
+  hasParent(index) {
+    return this.parentIndex(index) >= 0;
+  }
+
+  // Returns true if the node at index has a left child
+  hasLeftChild(index) {
+    return this.leftChildIndex(index) < this.items.length;
+  }
+
+  // Returns true if the node at index has a right child
+  hasRightChild(index) {
+    return this.rightChildIndex(index) < this.items.length;
+  }
+
+  // Bubble up the element at the index
+  bubbleUp() {
+    let index = this.items.length - 1;
+    while (
+      this.hasParent(index) &&
+      this.items[this.parentIndex(index)].priority > this.items[index].priority
+    ) {
+      this.swap(this.parentIndex(index), index);
+      index = this.parentIndex(index);
+    }
+  }
+
+  // Bubble down the element at the index
+  bubbleDown() {
+    let index = 0;
+    while (this.hasLeftChild(index)) {
+      let smallerChildIndex = this.leftChildIndex(index);
+      if (
+        this.hasRightChild(index) &&
+        this.items[this.rightChildIndex(index)].priority <
+          this.items[smallerChildIndex].priority
+      ) {
+        smallerChildIndex = this.rightChildIndex(index);
+      }
+
+      if (this.items[index].priority < this.items[smallerChildIndex].priority) {
+        break;
+      } else {
+        this.swap(index, smallerChildIndex);
+      }
+      index = smallerChildIndex;
+    }
+  }
+
+  // Enqueue element with a priority
+  enqueue(element, priority) {
+    this.items.push({ element, priority });
+    this.bubbleUp();
+  }
+
+  // Dequeue the element with the highest priority
+  dequeue() {
+    if (this.items.length === 0) {
+      throw new Error("PriorityQueue is empty");
+    }
+    const item = this.items[0];
+    this.items[0] = this.items[this.items.length - 1];
+    this.items.pop();
+    this.bubbleDown();
+    return item;
+  }
+
+  // Check if the priority queue is empty
+  isEmpty() {
+    return this.items.length === 0;
+  }
+}
+
+export default PriorityQueue;
---
+++ DirectedGraph/01.03/example03.js
@@ -0,0 +1,19 @@
+import DirectedGraph from "./DirectedGraph.js";
+
+let graph = new DirectedGraph();
+
+let nodeA = graph.addNode("A");
+let nodeB = graph.addNode("B");
+let nodeC = graph.addNode("C");
+let nodeD = graph.addNode("D");
+let nodeE = graph.addNode("E");
+
+graph.addEdge(nodeA, nodeB, 6);
+graph.addEdge(nodeA, nodeD, 1);
+graph.addEdge(nodeB, nodeD, 2);
+graph.addEdge(nodeB, nodeE, 2);
+graph.addEdge(nodeD, nodeE, 1);
+graph.addEdge(nodeB, nodeC, 5);
+graph.addEdge(nodeE, nodeC, 5);
+
+graph.dijkstra("A", "C");
