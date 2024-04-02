d82f9acfc194f8977351671a7ca43a4149fcdf54
julihocc <juliho.castillo@outlook.com>
2024-04-02 11:08:01 -0600

    Refactor directions.interface.ts

--- OOPShapeController/03.01/dist/directions.interface.js
+++ OOPShapeController/03.01/dist/directions.interface.js
@@ -1,26 +1,6 @@
 "use strict";
-// directions.interface.ts
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.Direction = void 0;
-// export interface Direction {
-//   deltax: number;
-//   deltay: number;
-// }
-// export interface Directions {
-//   up: Direction;
-//   down: Direction;
-//   left: Direction;
-//   right: Direction;
-// }
-// enum Direction {
-//   Up = "UP",
-//   Down = "DOWN",
-//   Left = "LEFT",
-//   Right = "RIGHT"
-// }
-// interface Directions {
-//   [key in Direction]?: { deltax: number, deltay: number };
-// }
 var Direction;
 (function (Direction) {
     Direction["Up"] = "UP";
--- OOPShapeController/03.01/src/directions.interface.ts
+++ OOPShapeController/03.01/src/directions.interface.ts
@@ -1,28 +1,3 @@
-// directions.interface.ts
-
-// export interface Direction {
-//   deltax: number;
-//   deltay: number;
-// }
-
-// export interface Directions {
-//   up: Direction;
-//   down: Direction;
-//   left: Direction;
-//   right: Direction;
-// }
-
-// enum Direction {
-//   Up = "UP",
-//   Down = "DOWN",
-//   Left = "LEFT",
-//   Right = "RIGHT"
-// }
-
-// interface Directions {
-//   [key in Direction]?: { deltax: number, deltay: number };
-// }
-
 export enum Direction {
   Up = "UP",
   Down = "DOWN",
