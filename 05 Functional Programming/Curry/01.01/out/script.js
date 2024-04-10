a1026c035e8721cd500411d2ebb6947516247ff3
julihocc <juliho.castillo@outlook.com>
2024-04-10 17:50:20 -0600

    Refactor curry function and fix formatting in script.js

--- 05 Functional Programming/Curry/01.01/out/script.js
+++ 05 Functional Programming/Curry/01.01/out/script.js
@@ -1,23 +1,22 @@
 "use strict";
 function curry(fn) {
-    return function curried(...args) {
-        if (args.length >= fn.length) {
-            return fn.apply(null, args);
-        }
-        else {
-            return function (...args2) {
-                return curried.apply(null, args.concat(args2));
-            };
-        }
-    };
+  return function curried(...args) {
+    if (args.length >= fn.length) {
+      return fn.apply(null, args);
+    } else {
+      return function (...args2) {
+        return curried.apply(null, args.concat(args2));
+      };
+    }
+  };
 }
 function add(a, b, c) {
-    return a + b + c;
+  return a + b + c;
 }
 const curriedAdd = curry(add);
 console.log(curriedAdd(1)(2)(3));
 function concat(a, b, c) {
-    return a + b + c;
+  return a + b + c;
 }
 const curriedConcat = curry(concat);
 console.log(curriedConcat("Hello")(" ")("World!"));
