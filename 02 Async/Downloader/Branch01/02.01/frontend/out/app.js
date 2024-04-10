041cfdc3d5d711e08ce9d127b69a2abde3e57ebb
julihocc <juliho.castillo@outlook.com>
2024-04-09 18:12:09 -0600

    Remove unused files and dependencies

---
+++ 02 Async/Downloader/Branch01/02.01/frontend/out/app.js
@@ -0,0 +1,53 @@
+"use strict";
+const fetchImageElement = document.getElementById("fetch-image");
+// document.getElementById("fetch-image").addEventListener("click", function () {
+fetchImageElement.addEventListener("click", function () {
+    fetch("http://localhost:3000/image")
+        .then((response) => {
+        const contentLength = response.headers.get("Content-Length");
+        if (!response.body || !contentLength) {
+            throw new Error("Browser does not support streaming response body");
+        }
+        const reader = response.body.getReader();
+        let receivedLength = 0; // received that many bytes at the moment
+        let chunks = []; // array of received binary chunks (comprises the body)
+        return new ReadableStream({
+            start(controller) {
+                function read() {
+                    reader
+                        .read()
+                        .then(({ done, value }) => {
+                        if (done) {
+                            controller.close();
+                            return;
+                        }
+                        chunks.push(value);
+                        receivedLength += value.length;
+                        const downloadProgressElement = document.getElementById("download-progress");
+                        // document.getElementById("download-progress").value =
+                        downloadProgressElement.value =
+                            (receivedLength / parseInt(contentLength)) * 100;
+                        controller.enqueue(value);
+                        read();
+                    })
+                        .catch((error) => {
+                        console.error("Stream reading failed:", error);
+                        controller.error(error);
+                    });
+                }
+                read();
+            },
+        });
+    })
+        .then((stream) => new Response(stream))
+        .then((response) => response.blob())
+        .then((blob) => {
+        const url = URL.createObjectURL(blob);
+        const img = document.createElement("img");
+        img.src = url;
+        // document.getElementById("image-container").appendChild(img);
+        const imageContainerElement = document.getElementById("image-container");
+        imageContainerElement.appendChild(img);
+    })
+        .catch((error) => console.error("Fetch failed:", error));
+});
---
+++ 02 Async/Downloader/Branch02/02.01/src/server.ts
@@ -0,0 +1,19 @@
+const express = require("express");
+const path = require("path");
+const cors = require("cors"); // Make sure to npm install cors
+
+const app = express();
+
+// Use CORS to allow requests from your frontend domain
+app.use(cors());
+
+app.get("/image", (req, res) => {
+  // Specify the path to your image
+  const imagePath = path.join(__dirname, "images/rocky.png");
+  // Set the Content-Type header based on the file type
+  res.type("image/*");
+  res.sendFile(imagePath);
+});
+
+const PORT = 3000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
