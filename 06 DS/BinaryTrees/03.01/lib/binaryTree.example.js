a98a3a5f67e55f1702e8a38e8b04ecbd18c80a89
julihocc <juliho.castillo@outlook.com>
2024-04-10 18:17:51 -0600

    Binary tree visualization done

---
+++ 06 DS/BinaryTrees/03.01/lib/binaryTree.example.js
@@ -0,0 +1,15 @@
+import BinaryTree from "./BinaryTree.js";
+const tree = new BinaryTree();
+tree.insert(5);
+tree.insert(3);
+tree.insert(7);
+tree.print();
+const subtree = new BinaryTree();
+subtree.insert(4);
+subtree.insert(6);
+subtree.insert(1);
+subtree.print();
+tree.insertSubtree(subtree);
+tree.print();
+tree.remove(3);
+tree.print();
---
+++ 06 DS/BinaryTrees/03.01/lib/script.js
@@ -0,0 +1,12 @@
+import BinaryTree from "./BinaryTree.js";
+const tree = new BinaryTree();
+tree.insert(5);
+tree.insert(3);
+tree.insert(7);
+const subtree = new BinaryTree();
+subtree.insert(4);
+subtree.insert(6);
+subtree.insert(1);
+tree.insertSubtree(subtree);
+const treeContainer = document.getElementById("tree-container");
+treeContainer.innerHTML = tree.getHTML();
