a01b57c63b5474b6ee88669a54dc1433d31cd2f5
julihocc <juliho.castillo@outlook.com>
2024-04-16 11:04:10 -0600

    Refactor React components and remove unused files and configurations

--- 08 React/ContactListTS/02.03/frontend/src/components/Contact/index.tsx
+++ 08 React/ContactListTS/02.03/frontend/src/components/Contact/index.tsx
@@ -2,8 +2,6 @@ import { Component } from "react";
 import styles from "./styles.module.css";
 import Phone from "../Phone";
 
-
-
 export default class Contact extends Component<ContactProps> {
   constructor(props: ContactProps) {
     super(props);
@@ -12,23 +10,26 @@ export default class Contact extends Component<ContactProps> {
     const { name, email, phone, onDeleteContact, favorite, onToggleFavorite } =
       this.props;
     return (
-      <div>
-        <li
-          className={`
+      <li
+        className={`
           ${styles.contact}
           ${favorite ? styles.favorite : ""}
         `}
-        >
+      >
+        {" "}
+        <div>
           <h4>Name: {name}</h4>
           {email && <p>Email: {email}</p>}
           {phone && <Phone phone={phone} />}
-        </li>
-        <button onClick={onDeleteContact}>Delete Contact</button>
-        <button onClick={onToggleFavorite}>
-          {favorite ? "Unmark" : "Mark"}
-        </button>
-      </div>
+        </div>
+        <div>
+          {" "}
+          <button onClick={onDeleteContact}>Delete Contact</button>
+          <button onClick={onToggleFavorite}>
+            {favorite ? "Unmark" : "Mark"}
+          </button>
+        </div>
+      </li>
     );
   }
 }
-
