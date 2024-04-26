0ef1ef8b8859d296b53c7750909a86cda5798c15
julihocc <juliho.castillo@outlook.com>
2024-04-25 18:05:23 -0600

    nav bar previous behavior has been mimicked with react routes

--- 08 React/ReactApp/01.10/contact-list-functional/src/main.jsx
+++ 08 React/ReactApp/01.10/contact-list-functional/src/main.jsx
@@ -3,11 +3,12 @@ import ReactDOM from "react-dom/client";
 import { App } from "./App.jsx";
 import "./index.css";
 import { DataProvider } from "./Context/DataProvider";
+import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <DataProvider>
-      <App />
+        <App />
     </DataProvider>
   </React.StrictMode>
 );
