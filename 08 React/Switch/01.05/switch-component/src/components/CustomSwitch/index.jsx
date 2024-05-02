ba3b0161cfc2612387586136f945f745d43395ae
julihocc <juliho.castillo@outlook.com>
2024-05-02 15:52:20 -0600

    has selector trick is not working

---
+++ 08 React/Switch/01.05/switch-component/src/components/CustomSwitch/index.jsx
@@ -0,0 +1,24 @@
+import styles from "./Switch.module.css";
+import { useState } from "react";
+
+function CustomSwitch({ onChange, left = false, right = false }) {
+  const [checked, setChecked] = useState(false);
+  console.log("left: ", left);
+  console.log("right: ", right);
+  const toggleSwitch = () => {
+    setChecked((prevChecked) => !prevChecked);
+    onChange && onChange();
+  };
+
+  return (
+    <div
+      className={`${styles.customSwitch} ${right ? styles.right : ""}`}
+      onClick={toggleSwitch}
+    >
+      <input type="checkbox" checked={checked} onChange={() => {}} />
+      <span className={styles.customSwitchSlider}></span>
+    </div>
+  );
+}
+
+export default CustomSwitch;
