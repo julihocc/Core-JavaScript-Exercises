696ab86357f2c780dd138d457e96b0cd6620be1a
julihocc <juliho.castillo@outlook.com>
2024-05-06 15:29:38 -0600

    refactor: Update App component to use Game component for Tic Tac Toe game

---
+++ 08 React/TicTacToe/01.02/tic-tac-toe/src/App.tsx
@@ -0,0 +1,12 @@
+import Game from "./components/Game";
+
+function App() {
+  return (
+    <>
+      <div className="h1">Tic Tac Toe 1.0</div>
+      <Game />
+    </>
+  );
+}
+
+export default App;
