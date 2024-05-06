70ca6f16bef651c25a5e9aba6dc83babeee60306
julihocc <juliho.castillo@outlook.com>
2024-05-06 09:13:02 -0600

    titles for items are aligned

---
+++ 08 React/NavigationDrawer/01.06/navigation-drawer/src/components/Sidenav/Sidenav.module.css.d.ts
@@ -0,0 +1,8 @@
+declare const classNames: {
+  readonly sidenav: "sidenav";
+  readonly open: "open";
+  readonly close: "close";
+  readonly sandwichClose: "sandwichClose";
+  readonly sandwichOpen: "sandwichOpen";
+};
+export = classNames;
---
+++ 08 React/NavigationDrawer/01.06/navigation-drawer/src/components/SidenavItem/SidenavItem.module.css.d.ts
@@ -0,0 +1,8 @@
+declare const classNames: {
+  readonly "sidenav-item": "sidenav-item";
+  readonly "sidenav-item-link": "sidenav-item-link";
+  readonly "sidenav-item-icon": "sidenav-item-icon";
+  readonly "sidenav-item-title": "sidenav-item-title";
+  readonly "sidenav-item-badge": "sidenav-item-badge";
+};
+export = classNames;
