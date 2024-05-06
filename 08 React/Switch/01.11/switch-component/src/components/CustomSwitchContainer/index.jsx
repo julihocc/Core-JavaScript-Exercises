022c805ae3c900ff2d9cf97a98f91283489e5538
julihocc <juliho.castillo@outlook.com>
2024-05-06 10:37:17 -0600

    new iteration

---
+++ 08 React/Switch/01.11/switch-component/src/components/CustomSwitchContainer/index.jsx
@@ -0,0 +1,19 @@
+import { useContext } from "react";
+import { ThemeContext } from "../ThemeContext";
+import CustomSwitch from "../CustomSwitch";
+
+function CustomSwitchContainer() {
+  const { toggleTheme } = useContext(ThemeContext);
+
+  return (
+    <div>
+      <CustomSwitch
+        onChange={toggleTheme}
+        leftIcon={"./erase.svg"}
+        rightIcon={"/cart.svg"}
+      />
+    </div>
+  );
+}
+
+export default CustomSwitchContainer;
