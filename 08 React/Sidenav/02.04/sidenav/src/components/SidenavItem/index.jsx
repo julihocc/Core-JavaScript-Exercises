897c5d0944dfb0a613210b23b93492957594bc6e
julihocc <juliho.castillo@outlook.com>
2024-05-08 12:59:57 -0600

    refactor: Update SidenavItem module CSS to change background color on hover

--- 08 React/Sidenav/02.04/sidenav/src/components/SidenavItem/index.jsx
+++ 08 React/Sidenav/02.04/sidenav/src/components/SidenavItem/index.jsx
@@ -1,5 +1,6 @@
 import styles from "./SidenavItem.module.css";
 import { useSidenavState } from "../SidenavContext";
+import { useRef } from "react";
 
 const SidenavItem = ({
   title,
@@ -11,14 +12,30 @@ const SidenavItem = ({
   // const { closingMode } = useContext(SidenavContext);
   const sideNavState = useSidenavState();
   const { closingMode, isOpen } = sideNavState;
+  const containerRef = useRef(null);
 
   console.log(`closingMode: ${closingMode}`);
+
+  const handleMouseEnter = () => {
+    if (containerRef.current) {
+      // containerRef.current.style.zIndex = 1;
+      containerRef.current.style.backgroundColor = "#ff5722";
+    }
+  };
+
+  const handleMouseLeave = () => {
+    if (containerRef.current) {
+      // containerRef.current.style.zIndex = 0;
+      containerRef.current.style.backgroundColor = "pink";
+    }
+  };
 
   return (
     <div className={`${styles["sidenav-item-cage"]}`}>
       <div
         className={`${styles["sidenav-item"]} ${styles[closingMode]} ${isOpen ? styles.open : styles.close}`}
         onClick={onClick}
+        ref={containerRef}
       >
         <div className={`${styles["sidenav-item-link"]} `}>
           {icon && (
@@ -40,6 +57,8 @@ const SidenavItem = ({
       </div>
       <div
         className={`${styles["sidenav-item-children"]} ${styles[closingMode]}`}
+        onMouseEnter={handleMouseEnter}
+        onMouseLeave={handleMouseLeave}
       >
         {children}
       </div>
