b57e8ab70017fa2b40288d56cb21fefd6a404bac
julihocc <juliho.castillo@outlook.com>
2024-05-08 18:13:36 -0600

    new iteration

---
+++ 08 React/TicTacToe/01.06/tic-tac-toe/src/App.tsx
@@ -0,0 +1,12 @@
+import GameContainer from "./components/GameContainer";
+
+function App() {
+  return (
+    <>
+      <div className="h1">Tic Tac Toe 1.0</div>
+      <GameContainer />
+    </>
+  );
+}
+
+export default App;
---
+++ 08 React/TicTacToe/01.06/tic-tac-toe/src/types/Square.module.d.ts
@@ -0,0 +1,4 @@
+type SquareProps = {
+  value: Chip;
+  onClick: () => void;
+};
