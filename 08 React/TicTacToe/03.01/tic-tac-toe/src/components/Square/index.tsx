168f124e317053d382688d604f72cf43223ae494
julihocc <juliho.castillo@outlook.com>
2024-05-13 08:40:13 -0600

    chore: Update npm dependencies for @react-spring to version 9.7.3

--- 08 React/TicTacToe/03.01/tic-tac-toe/src/components/Square/index.tsx
+++ 08 React/TicTacToe/03.01/tic-tac-toe/src/components/Square/index.tsx
@@ -1,16 +1,38 @@
 import { Box, Button } from "@radix-ui/themes";
 import { useGameStore } from "../../store";
+import { animated, useSpring } from "@react-spring/web";
+import { useState } from "react";
+import styles from "./Square.module.css";
 
-export default function Square(props: SquareProps) {
+export default function Square({ value, onClick }: SquareProps) {
   const gameState = useGameStore((state) => state);
+  const [isClicked, setIsClicked] = useState(false);
+
+  // animate rotation about the z-axis
+  const rotation = useSpring({
+    transform: isClicked
+      ? "perspective(600px) rotateY(180deg)"
+      : "perspective(600px) rotateY(0deg)",
+    config: { mass: 10, tension: 90, friction: 10 },
+  });
+
+  const handleClick = () => {
+    setIsClicked(true);
+    onClick();
+  };
+
   return (
-    <Box as="div" width="32px" height="32px" mx="1" onClick={props.onClick}>
+    <Box as="div" width="32px" height="32px" mx="1" onClick={handleClick}>
       <Button style={{ width: "32px" }} variant="outline">
-        {props.value === 0
-          ? gameState.iconPlayer0()
-          : props.value === 1
-          ? gameState.iconPlayer1()
-          : null}
+        {value === 0 ? (
+          <animated.div style={rotation} className={styles.parent}>
+            {gameState.iconPlayer0()}{" "}
+          </animated.div>
+        ) : value === 1 ? (
+          <animated.div style={rotation} className={styles.parent}>
+            {gameState.iconPlayer1()}{" "}
+          </animated.div>
+        ) : null}
       </Button>
     </Box>
   );
