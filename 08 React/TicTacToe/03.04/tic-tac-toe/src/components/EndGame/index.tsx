11764b187ff62ac69ace3740ab8efce9abc09303
julihocc <juliho.castillo@outlook.com>
2024-05-13 15:52:55 -0600

    refactor: Update EndGame component to use Confetti for celebration

--- 08 React/TicTacToe/03.04/tic-tac-toe/src/components/EndGame/index.tsx
+++ 08 React/TicTacToe/03.04/tic-tac-toe/src/components/EndGame/index.tsx
@@ -1,8 +1,9 @@
 // import * as Dialog from "@radix-ui/react-dialog";
-import { AlertDialog, Button } from "@radix-ui/themes";
+import { AlertDialog, Flex, Box, Button, Heading } from "@radix-ui/themes";
 import { useState, useEffect } from "react";
 import { useGameStore } from "../../store";
-import { animated, useTransition, useSpring } from "@react-spring/web";
+import { animated, useTransition } from "@react-spring/web";
+import Confetti from "../Confetti";
 
 export default function EndGame() {
   const gameState = useGameStore((state) => state);
@@ -19,28 +20,10 @@ export default function EndGame() {
     }
   }, [gameState.winner, gameState.tie]);
 
-  // const transition = useTransition(openModal, {
-  //   from: {
-  //     opacity: 0,
-  //     y: -50,
-  //   },
-  //   enter: {
-  //     opacity: 0.1,
-  //     y: 0,
-  //   },
-  //   leave: {
-  //     opacity: 0,
-  //     y: -50,
-  //   },
-  //   config: {
-  //     duration: 2000,
-  //   },
-  // });
-
   const configObj = {
     from: {
       opacity: 0,
-      y: -50,
+      y: -10,
     },
     enter: {
       opacity: 1,
@@ -48,7 +31,7 @@ export default function EndGame() {
     },
     leave: {
       opacity: 0,
-      y: 50,
+      y: 10,
     },
     config: {
       duration: 500,
@@ -63,11 +46,39 @@ export default function EndGame() {
       <AlertDialog.Root open={openModal} onOpenChange={setOpenModal}>
         {transition((styles, item) =>
           item ? (
-            <AnimatedAlertDialogContent style={styles}>
-              {JSON.stringify(styles)}
-              <AlertDialog.Cancel onClick={cancelModal}>
-                <Button>Close</Button>
-              </AlertDialog.Cancel>
+            <AnimatedAlertDialogContent forceMount style={styles}>
+              <animated.div style={styles}>
+                <Flex direction="column" gap="3">
+                  <Box>
+                    {gameState.winner !== null ? (
+                      gameState.winner === 0 ? (
+                        <div>
+                          <Heading size="3" as="h3">
+                            Player {gameState.iconPlayer0()} won
+                          </Heading>
+                        </div>
+                      ) : (
+                        <Heading size="3" as="h3">
+                          Player {gameState.iconPlayer1()} won
+                        </Heading>
+                      )
+                    ) : gameState.tie ? (
+                      <Heading size="3" as="h3">
+                        It's a tie
+                      </Heading>
+                    ) : (
+                      <Heading size="3" as="h3">
+                        Thanks for playing
+                      </Heading>
+                    )}
+                  </Box>
+                  <Box>
+                    <AlertDialog.Cancel onClick={cancelModal}>
+                      <Button>Close</Button>
+                    </AlertDialog.Cancel>
+                  </Box>
+                </Flex>
+              </animated.div>
             </AnimatedAlertDialogContent>
           ) : null
         )}
@@ -75,7 +86,7 @@ export default function EndGame() {
     );
   };
 
-  const AnimatedDialog = animated(ExternalComponent);
-
-  return <AnimatedDialog />;
+  // const AnimatedDialog = animated(ExternalComponent);
+  // return <AnimatedDialog />;
+  return <ExternalComponent />;
 }
