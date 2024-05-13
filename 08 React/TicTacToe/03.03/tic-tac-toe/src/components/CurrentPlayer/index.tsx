065eeab5dc53d66a2521d0dbfb2918cdfde4123f
julihocc <juliho.castillo@outlook.com>
2024-05-13 11:18:46 -0600

    new iteration

---
+++ 08 React/TicTacToe/03.03/tic-tac-toe/src/components/CurrentPlayer/index.tsx
@@ -0,0 +1,38 @@
+// import { useGameState } from "../Context";
+// import { FC } from "react";
+import { useGameStore } from "../../store";
+import { Heading, Container, Box } from "@radix-ui/themes";
+
+const CurrentPlayer = () => {
+  // const gameState = useGameState();
+  const gameState = useGameStore((state) => state);
+  const currentPlayer = gameState.currentPlayer;
+  const winner = gameState.winner;
+  const draft = useGameStore((state) => state.draft);
+  let forRender = null;
+
+  if (winner !== null) {
+    forRender = () => (
+      <Heading as="h3">Winner: {gameState.iconPlayer0()}</Heading>
+    );
+  } else if (draft) {
+    forRender = () => <Heading as="h3">It's a draft 🙃</Heading>;
+  } else {
+    forRender = () => (
+      <Heading as="h3">
+        Waiting for:{" "}
+        {currentPlayer === 0
+          ? gameState.iconPlayer0()
+          : gameState.iconPlayer1()}
+      </Heading>
+    );
+  }
+
+  return (
+    <Box>
+      <Container size="1">{forRender()}</Container>
+    </Box>
+  );
+};
+
+export default CurrentPlayer;
