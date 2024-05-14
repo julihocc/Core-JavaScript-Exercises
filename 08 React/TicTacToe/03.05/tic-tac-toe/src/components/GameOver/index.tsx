6aa01714ff013c4eabfc70a59999a9ceeb5ed808
julihocc <juliho.castillo@outlook.com>
2024-05-14 07:50:27 -0600

    refactor: Update GameOver component to display winner or tie message

--- 08 React/TicTacToe/03.05/tic-tac-toe/src/components/GameOver/index.tsx
+++ 08 React/TicTacToe/03.05/tic-tac-toe/src/components/GameOver/index.tsx
@@ -1,12 +1,14 @@
 // import * as Dialog from "@radix-ui/react-dialog";
 import { AlertDialog, Flex, Box, Button, Heading } from "@radix-ui/themes";
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 import { useGameStore } from "../../store";
 import { animated, useTransition } from "@react-spring/web";
 
 export default function GameOver() {
   const gameState = useGameStore((state) => state);
   const [openModal, setOpenModal] = useState(false);
+  const [message, setMessage] = useState<JSX.Element | null>(null);
+  const prevMessage = useRef<JSX.Element | null>(null);
 
   const cancelModal = () => {
     gameState.dispatch({ type: "RESET" });
@@ -37,6 +39,41 @@ export default function GameOver() {
     },
   };
 
+  useEffect(() => {
+    prevMessage.current = message;
+  }, [message]);
+
+  useEffect(() => {
+    let _message;
+    if (gameState.winner !== null) {
+      if (gameState.winner === 0) {
+        _message = (
+          <div>
+            <Heading size="3" as="h3">
+              Player {gameState.iconPlayer0()} won
+            </Heading>
+          </div>
+        );
+      } else {
+        _message = (
+          <Heading size="3" as="h3">
+            Player {gameState.iconPlayer1()} won
+          </Heading>
+        );
+      }
+    } else if (gameState.tie) {
+      _message = (
+        <Heading size="3" as="h3">
+          It's a tie
+        </Heading>
+      );
+    } else {
+      // _message = null;
+      _message = prevMessage.current;
+    }
+    setMessage(_message);
+  }, [gameState]);
+
   const transition = useTransition(openModal, configObj);
   const AnimatedAlertDialogContent = animated(AlertDialog.Content);
 
@@ -48,29 +85,7 @@ export default function GameOver() {
             <AnimatedAlertDialogContent forceMount style={styles}>
               <animated.div style={styles}>
                 <Flex direction="column" gap="3">
-                  <Box>
-                    {gameState.winner !== null ? (
-                      gameState.winner === 0 ? (
-                        <div>
-                          <Heading size="3" as="h3">
-                            Player {gameState.iconPlayer0()} won
-                          </Heading>
-                        </div>
-                      ) : (
-                        <Heading size="3" as="h3">
-                          Player {gameState.iconPlayer1()} won
-                        </Heading>
-                      )
-                    ) : gameState.tie ? (
-                      <Heading size="3" as="h3">
-                        It's a tie
-                      </Heading>
-                    ) : (
-                      <Heading size="3" as="h3">
-                        Thanks for playing
-                      </Heading>
-                    )}
-                  </Box>
+                  <Box>{message}</Box>
                   <Box>
                     <AlertDialog.Cancel onClick={cancelModal}>
                       <Button>Close</Button>
