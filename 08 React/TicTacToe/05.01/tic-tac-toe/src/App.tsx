50b83fd2f4b7df57fad468d4e4afe45d5ded0a5c
julihocc <juliho.castillo@outlook.com>
2024-05-16 13:19:27 -0600

    refactor: Improve structure and functionality of Tic Tac Toe game

--- 08 React/TicTacToe/05.01/tic-tac-toe/src/App.tsx
+++ 08 React/TicTacToe/05.01/tic-tac-toe/src/App.tsx
@@ -3,41 +3,16 @@ import {
   createBrowserRouter,
   createRoutesFromElements,
   Route,
-  Link,
-  Outlet,
   RouterProvider,
 } from "react-router-dom";
 
-import { Flex, Grid, DropdownMenu, Button, Container } from "@radix-ui/themes";
 import Game from "./components/Game";
 import Header from "./components/Header";
 import Stats from "./components/Stats";
 import Board from "./components/Board";
 
-const Root = () => (
-  <>
-    <DropdownMenu.Root>
-      <DropdownMenu.Trigger>
-        <Button>Open</Button>
-      </DropdownMenu.Trigger>
-      <DropdownMenu.Content>
-        <DropdownMenu.Item>
-          <Link to="/">Home</Link>
-        </DropdownMenu.Item>
-        <DropdownMenu.Item>
-          <Link to="/game">Game</Link>
-        </DropdownMenu.Item>
-        <DropdownMenu.Item>
-          <Link to="/stats">Stats</Link>
-        </DropdownMenu.Item>
-      </DropdownMenu.Content>
-    </DropdownMenu.Root>
-    <Outlet />
-  </>
-);
-
 const routesFromElements = createRoutesFromElements(
-  <Route path="/" element={<Root />}>
+  <Route path="/" element={<Header />}>
     <Route index element={<Board />} />
     <Route path="game" element={<Game />} />
     <Route path="stats" element={<Stats />} />
@@ -47,10 +22,7 @@ const routesFromElements = createRoutesFromElements(
 const router = createBrowserRouter(routesFromElements);
 
 const App: FC = () => (
-  <Container>
-    <Header />
     <RouterProvider router={router} />
-  </Container>
 );
 
 export default App;
--- 08 React/TicTacToe/05.01/tic-tac-toe/src/components/Header/index.tsx
+++ 08 React/TicTacToe/05.01/tic-tac-toe/src/components/Header/index.tsx
@@ -1,5 +1,35 @@
-import { Heading, Section, Switch, Flex } from "@radix-ui/themes";
+import {
+  Heading,
+  Section,
+  Switch,
+  Flex,
+  DropdownMenu,
+  Button,
+} from "@radix-ui/themes";
 import { useThemeStore } from "../../stores/themeStore";
+import { Link, Outlet } from "react-router-dom";
+import { ChevronDownIcon } from "@radix-ui/react-icons";
+
+const Goto = () => (
+  <DropdownMenu.Root>
+    <DropdownMenu.Trigger>
+      <Button>
+        Go to <ChevronDownIcon />
+      </Button>
+    </DropdownMenu.Trigger>
+    <DropdownMenu.Content>
+      <DropdownMenu.Item>
+        <Link to="/">Home</Link>
+      </DropdownMenu.Item>
+      <DropdownMenu.Item>
+        <Link to="/game">Game</Link>
+      </DropdownMenu.Item>
+      <DropdownMenu.Item>
+        <Link to="/stats">Stats</Link>
+      </DropdownMenu.Item>
+    </DropdownMenu.Content>
+  </DropdownMenu.Root>
+);
 
 export default function Header() {
   const theme = useThemeStore((state) => state.theme);
@@ -10,13 +40,17 @@ export default function Header() {
   };
 
   return (
-    <Section size="4">
-      <Flex justify="center" align="center" gapX="9">
-        <Heading as="h1" size="9">
-          Tic Tac Toe
-        </Heading>
-        <Switch onClick={handleClick}></Switch>
-      </Flex>
-    </Section>
+    <Flex direction="column" justify="center" align="center">
+      <Section size="4">
+        <Flex justify="center" align="center" gapX="9">
+          <Heading as="h1" size="9">
+            Tic Tac Toe
+          </Heading>
+          <Goto />
+          <Switch onClick={handleClick}></Switch>
+        </Flex>{" "}
+      </Section>
+      <Outlet />
+    </Flex>
   );
 }
