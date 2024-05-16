ec391da24c0c4d28314db81cc2bf0dfb9a52da43
julihocc <juliho.castillo@outlook.com>
2024-05-16 10:17:05 -0600

    chore: Update themeStore to use zustand's create function

--- 08 React/TicTacToe/04.06/tic-tac-toe/src/stores/themeStore/index.tsx
+++ 08 React/TicTacToe/04.06/tic-tac-toe/src/stores/themeStore/index.tsx
@@ -1,11 +1,11 @@
-import create from "zustand";
+import { create } from "zustand";
 
 type ThemeStore = {
-  theme: "light" | "inherit" | "dark" | undefined;
-  setTheme: (theme: string) => void;
+  theme: Theme;
+  setTheme: (theme: Theme) => void;
 };
 
 export const useThemeStore = create<ThemeStore>((set) => ({
   theme: "light",
-  setTheme: (theme) => set({ theme }),
+  setTheme: (theme: Theme) => set({ theme }),
 }));
--- 08 React/TicTacToe/04.06/tic-tac-toe/src/types/globals.module.d.ts
+++ 08 React/TicTacToe/04.06/tic-tac-toe/src/types/globals.module.d.ts
@@ -10,3 +10,4 @@ type Chip = 0 | 1 | null;
 type Step = 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9;
 type Player = 0 | 1;
 type DateAsNumber = number;
+type Theme = "light" | "inherit" | "dark" | undefined;
